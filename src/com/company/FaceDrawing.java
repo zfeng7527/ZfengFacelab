d398fd4e39a4f54498aba1e3d9628aae224e4558
BT_1N3_20@bktech.local <BT_1N3_20@bktech.local>
2018-12-04 08:30:40 -0500

    Initial commit

---
+++ src/com/company/FaceDrawing.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class FaceDrawing {
+
+    FaceDrawing(String[][] faceFeatures)
+    }
+
+        void edit(String replace, int row, int column){
+
+        }
+
+        void fill(String str){
+        String[0][1] = str;
+        }
+
+        String toString();
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String[][] face = new String[5][8];
+	JackOLantern face1 = new JackOLantern(face);
+
+	    face1.fill("x");
+	    face1.edit("",0,0);
+        face1.edit("",4,7);
+        face1.edit("",0,7);
+        face1.edit("",4,0);
+
+        face1.edit("",1,2);
+        face1.edit("",1,5);
+        face1.edit("",2,3);
+        face1.edit("",2,4);
+        face1.edit("",3,1);
+        face1.edit("",3,2);
+        face1.edit("",3,3);
+        face1.edit("",3,4);
+        face1.edit("",3,5);
+        face1.edit("",3,6);
+        System.out.println(face1);
+
+    }
+}
